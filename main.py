e03a747badf79d6006454f6eb31355ed389b0f13
Kaindall <gustavozanete2@gmail.com>
2022-10-20 17:01:26 -0300

    the first pre-visualization of all covid's cases on Brazil and starting to filter the new cases to I continue in my home

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ import re
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.io as pio
 import plotly.graph_objects as go
 
+
+#open the sheet and set some columns to date
 df = pd.read_csv("covid_19_data.csv", parse_dates=["ObservationDate", "Last Update"])
 
-print (df.tail())
+#rename all columns to get a default template
+df.columns = [re.sub(r"[/| ]", "", col).lower() for col in df.columns]
+
+#filtering just brazilian cases and plotting them
+df_br = df.loc[(df["countryregion"] == "Brazil") & (df["confirmed"] > 0)]
+fig = px.line(df_br, "observationdate", "confirmed", title="Casos Confirmados")
+
+
+df_br["newcases"] = list(map(
+    lambda x: 0 if (x==0) \
+        else df_br["confirmed"].iloc[x] - df_br["confirmed"].iloc[x]-1,
+        np.arange(df_br.shape[0])
+))
